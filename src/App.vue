e4608e3b8e25083bce2fdc4cd5e4a27d152105e4
David Claridge <daave@daave.com>
2020-10-25 00:07:18 +0000

    Render a ShapeDiver model in a vue component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Building />
+    <Building ticket="20f9d15ecb236b79c4c342f2714558f571d34ae17fc3678f09a4e6742f28045119010401d66d47bd2f4c0a4a5ae92be36c560606effd0a98ec6ebdcec3975a5fd947d156338ff31e4a3c142a7eb4aa864f62d3db3938be957ab118903125b434296303731c19772d5a7fc89ae42348eba26888a80fdd-75013a6ebb275765f424fa8c2b7976a5" modelViewUrl="eu-central-1" />
   </div>
 </template>
 
--- src/components/Building.vue
+++ src/components/Building.vue
@@ -1,10 +1,69 @@
-<div id="sdv-container"></div>
+<template>
+    <div class="building">
+        <div ref="shapediver-viewport" style="width:100%; height:400px;"></div>
+        <div ref="param-controls">
+            <label for="numFloors">Num Floors:</label>
+            <input v-model="modelParams.numFloors" type="number" step="1" id="numFloors">
+            <p>Num Floors is: {{ modelParams.numFloors }}</p>
+
+            <label for="flipStructureOrientation">Flip Structure Orientation:</label>
+            <input v-model="modelParams.flipStructureOrientation" type="checkbox" id="flipStructureOrientation">
+            <p>Flip Structure Orientation is: {{ modelParams.flipStructureOrientation }}</p>
+
+            <label for="structuralSystem">Structural System:</label>
+            <select v-model="modelParams.structuralSystem" id="structuralSystem">
+                <option>Beams in Both Directions</option>
+                <option>Beams in One Direction</option>
+                <option>Beams and Girders</option>
+                <option>Point Supported</option>
+            </select>
+            <p>Structural System is: {{ modelParams.structuralSystem }}</p>
+        </div>
+    </div>
+</template>
 
 <script>
 export default {
   name: 'Building',
-  props: {
-  }
+  data() {
+    return {
+        modelParams: {
+            numFloors: 3,
+            flipStructureOrientation: false,
+            structuralSystem: "Beams in Both Directions"
+        }
+    }
+  },
+  async mounted() {
+    let settings = {
+        showSceneMode: 1,
+        container: this.$refs["shapediver-viewport"]
+    }
+    this.shapeDiverAPI = new window.SDVApp.ParametricViewer(settings);
+
+    let vm = this;
+    await this.shapeDiverAPI.plugins.registerCommPluginAsync({
+        // ticket of the model as shown on app.shapediver.com
+        ticket: vm.ticket,
+        // URL of the ShapeDiver backend system used
+        modelViewUrl: vm.modelViewUrl,
+        // runtime id to use for this CommPlugin (you might register several)
+        runtimeId: 'BuildingCommPlugin',
+        // the following setting tells the viewer to wait with loading of geometry
+        deferGeometryLoading: true
+    });
+    console.log('ShapeDiver CommPlugin successfully loaded');
+    let params = await this.shapeDiverAPI.parameters.get().data;
+    console.log('Available model parameters', params);
+    // finally show the scene
+    await this.shapeDiverAPI.plugins.refreshPluginAsync('BuildingCommPlugin');
+    await this.shapeDiverAPI.updateSettingAsync('scene.show', true);
+    console.log("showing scene")
+  },
+  /*watch: {
+      modelParams: this.shapeDiverAPI.parameters.updateAsync(this.modelParams)
+  },*/
+  props: ["ticket", "modelViewUrl"],
 }
 </script>
 
